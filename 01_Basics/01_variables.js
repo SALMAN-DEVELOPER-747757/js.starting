8e922c195ae1797f177573767df056bcfdbf09b8
Berlin <salmankhantecwork@gmail.com>
2026-05-10 07:35:06 +0000

    Differents variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=747757
+let accountEmail="salmna@gmail.com"
+var accountPassword="3423423"
+accountCity="swabi"
+let accountState;
+
+accountEmail="sas@gmail"
+accountPassword="2323"
+accountCity="mardan"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
