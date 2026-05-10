c5f55c5ac26efffdf66651b71fa4414c2eddd121
Berlin <salmankhantecwork@gmail.com>
2026-05-10 07:45:34 +0000

    Datatypes in js

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,28 @@
+"use strict";   //behave like new version of js code  
+// alert(3-2)    //this mean i'm using node js not browser only browser can do alert thing i console
+
+console.log(10*10); 
+console.log(
+    "Berlin")            //code readability is compulsory in coding. Mean can understant everyone the code
+
+    let name="Salman"
+    let age=21
+    let isloggedIn=false
+
+    //DATATYPE
+//Number =>2 Range of number to power 53.
+//bigInt => we use it for big number very big.
+//string =>  "".
+//Boolean => it's for true or false.
+//null =>Means stand Alone value. Also null is an object.
+//Undefined=>it's we declare something without giving the value. for example: let city;
+//Symbol=>For unique values.
+
+
+   //OBJECT
+ console.log(typeof null)   //its type is object
+ console.log(typeof undefined)  //its type is undefined.
+ console.log(typeof Number)   //it's type is function
+ console.log(typeof "")   //Double coots means the type is string  
+ console.log(typeof Boolean) //it's type is also function
+ console.log(typeof Date)  //It's type is also functon
