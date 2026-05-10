ab283eb3092148327f06ab0a2746a3ed5e35f0ee
Berlin <salmankhantecwork@gmail.com>
2026-05-10 10:35:59 +0000

    dataType summary

---
+++ 01_Basics/05_Datatypes-summary.js
@@ -0,0 +1,37 @@
+
+// Premitive 
+// 7:types=>Number,string,Boolean,Null,Undifined,Symbol,BigInt
+
+
+const score=1000;
+const Name="Salman";
+
+
+const scoreValue=100.2;
+const isloggedIn=false;
+const outsideTemp=null;
+let userEamil;
+const id=Symbol('1234');
+const AnotherId=Symbol('1213')
+
+console.log(id==AnotherId);
+
+const bigNumber=1424225223n
+
+// Non-Primitive(Reference)
+// Array,Objects,Function 
+const Capitals=["Berlin","Tokyo","paris","Rome"];
+
+let myObj={
+    name:"Salman",
+    age:21,
+    Gender:"Male"
+}
+
+const myFunctoin =function(){
+    console.log("Hello world");
+}
+
+console.log(typeof AnotherId);
+
+
