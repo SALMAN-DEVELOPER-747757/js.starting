88ae8ee845ed82a92229740a77dfc3ff05ceff40
Berlin <salmankhantecwork@gmail.com>
2026-05-10 07:47:01 +0000

    conversion operation in js

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,53 @@
+let score=123;
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let valueInnumber=Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);       
+
+//true=>convert into 1
+//false=>convert into 0
+//""=>convert into string
+//"salman 1223"=>NaN means Not a number we use string and number combine that is wrong.
+// let isloggedIn="salman";
+// let BooleanisLoggedin=Boolean(isloggedIn);
+// console.log(BooleanisLoggedin);
+//1=>true
+//0=false
+//""==>false
+//"salman"==>true
+
+let someNumber=74774;
+let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+    //    ***********Operatation******************
+    let value=3;
+    let negvalue=-value;
+    console.log(negvalue)
+    // console.log(2+2)
+    //     console.log(2-2)
+    //         console.log(2*2)
+    //             console.log(2**3)
+    //                 console.log(2/2)
+    //                     console.log(2%3)
+    // let str1=12
+    // let str2=33
+    // let str3=str1+str2;
+    // console.log(str3)
+    // console.log("2"+3)
+    //     console.log("2"+3+3)
+    //         console.log(2+2+"3")
+    //             console.log("2+2"+3)
+
+    //                 console.log(2+3*4%2)
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+++gameCounter
+console.log(gameCounter);
